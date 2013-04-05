5e66faf32272a4481580903dbe9c426e3158f577
Gary Malouf <gary@garymalouf.com>
2013-04-04 23:16:02 -0400

    Update project.clj to work lein 2 and storm 0.8.x

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject storm/storm-kestrel "0.7.0"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :javac-options {:debug "true" :fork "true"}
+(defproject storm/storm-kestrel "0.7.0-mx-1-SNAPSHOT"
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
+  :javac-options {:debug "true"}
   :dependencies [[kestrel-thrift-java-bindings "2.2.0"]]
-  :dev-dependencies [[storm "0.7.0"]]
-  :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
-)
+  :profiles {:dev
+              {:dependencies [[storm "0.8.0"]],
+               :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]}}
+  :min-lein-version "2.0.0")
